6a69833b80d9a10ef7c01217754829c00d4d5d6c
lyon <rethinkt@163.com>
2018-07-14 09:51:27 +0800

    update some code

--- java-9/java-9-base/src/main/java/com/andy/http/Http2Examples.java
+++ java-9/java-9-base/src/main/java/com/andy/http/Http2Examples.java
@@ -19,21 +19,16 @@ public class Http2Examples {
     public static void test1() throws Exception {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpResponse httpResponse = httpClient.send(HttpRequest.newBuilder(new URI("http://www.baidu.com")).GET().build(),
-                HttpResponse.BodyHandler.asString()
-        );
+                HttpResponse.BodyHandler.asString());
         int code = httpResponse.statusCode();
         System.out.println(code);
         System.out.println(httpResponse.body().toString());
     }
 
     public static void test2() throws Exception {
-
         HttpClient client = HttpClient.newHttpClient();
-
         HttpRequest req = HttpRequest.newBuilder(URI.create("http://www.baidu.com"))
-                        .header("User-Agent","Java")
-                        .GET()
-                        .build();
+                        .header("User-Agent","Java").GET().build();
         HttpResponse<String> resp = client.send(req, HttpResponse.BodyHandler.asString());
         System.out.println(resp.body());
     }
