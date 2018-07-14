a2000f8072092c44598b4a82d9f106dc23130342
lyon <rethinkt@163.com>
2018-07-14 09:38:02 +0800

    update some code

---
+++ java-9/java-9-base/src/main/java/com/andy/collection/ListNews.java
@@ -0,0 +1,21 @@
+package com.andy.collection;
+
+import java.util.List;
+
+/**
+ * @Author: Mr.lyon
+ * @CreateBy: 2018-07-14 09:36
+ **/
+public class ListNews {
+
+    public static void main(String[] args) {
+        List<String> list = List.of("aa", "bb", "cc");
+
+        List<Integer> entityList = List.of();
+
+        System.out.println(list);
+        System.out.println(entityList);
+
+    }
+
+}
