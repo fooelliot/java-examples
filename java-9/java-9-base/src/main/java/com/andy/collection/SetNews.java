a2235c350173edd7bc5523ee23de0de0e3c9190e
lyon <rethinkt@163.com>
2018-07-14 09:39:56 +0800

    update some code

--- java-9/java-9-base/src/main/java/com/andy/collection/SetNews.java
+++ java-9/java-9-base/src/main/java/com/andy/collection/SetNews.java
@@ -1,14 +1,13 @@
 package com.andy.collection;
 
-import java.util.List;
+import java.util.Set;
 
 /**
  * java9新特性
  * @Author: Mr.lyon
  * @CreateBy: 2018-07-14 09:36
  **/
-public class ListNews {
-
+public class SetNews {
 
     /**
      * 除了更短和更好阅读之外，这些方法也可以避免您选择特定的集合实现。 事实上，从工厂方法返回已放入数个元素的集合实现是高度优化的。这是可能的，因为它们是不可变的：在创建后，继续添加元素到这些集合会导致 “UnsupportedOperationException”
@@ -16,12 +15,11 @@ public class ListNews {
      */
 
     public static void main(String[] args) {
-        List<String> list = List.of("aa", "bb", "cc");
+        Set<String> set = Set.of("aa", "bb", "cc");
+        Set<Integer> entitySet = Set.of();
 
-        List<Integer> entityList = List.of();
-
-        System.out.println(list);
-        System.out.println(entityList);
+        System.out.println(set);
+        System.out.println(entitySet);
 
     }
 
